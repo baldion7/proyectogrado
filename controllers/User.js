da6783003a20b3dc001fdb17c4c73fb0055e37b4
Kevin <baldionkevin8@gmail.com>
2023-03-05 20:52:24 -0500

    Terminacion de las rutas del login y las demas rutas y tambien auth

--- controllers/User.js
+++ controllers/User.js
@@ -1,18 +1,95 @@
 import User from "../models/UserModal.js";
+import argon2 from "argon2";
 
-export const getUser =(req,res)=>{
-
+export const getUser = async (req, res) => {
+    try {
+        const response = await User.findAll({
+            attributes: ['id', 'name', 'lastname', 'email', 'password']
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 };
 
-export const getUserByid =(req,res)=>{
-
+export const getUserByid = async (req, res) => {
+    try {
+        const response = await User.findOne({
+            attributes: ['id', 'name', 'lastname', 'email', 'password'],
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 };
-export const createUser =(req,res)=>{
-
+export const createUser = async (req, res) => {
+    const {name, lastname, email, password, confPassword, role} = req.body;
+    if (password !== confPassword) return res.status(400).json({msg: "Password dan Confirm Password tidak cocok"});
+    const hashPassword = await argon2.hash(password);
+    try {
+        await User.create({
+            name: name,
+            lastname: lastname,
+            email: email,
+            password: hashPassword,
+            role: role
+        });
+        res.status(201).json({msg: "Register Berhasil"})
+    } catch (error) {
+        res.status(500).json({msg: error.message + "none"});
+    }
 };
-export const updateUser =(req,res)=>{
+export const updateUser = async (req, res) => {
 
+    const user = await User.findOne({
+        where: {
+            id: req.params.id
+        }
+    });
+    if (!user) return res.status(404).json({msg: "User tidak ditemukan"});
+    const {name, lastname, email, password, confPassword, role} = req.body;
+    let hashPassword;
+    if (password === "" || password === null) {
+        hashPassword = user.password
+    } else {
+        hashPassword = await argon2.hash(password);
+    }
+    if (password !== confPassword) return res.status(400).json({msg: "Password dan Confirm Password tidak cocok"});
+    try {
+        await User.update({
+            name: name,
+            lastname: lastname,
+            email: email,
+            password: hashPassword,
+            role: role
+        },{
+            where:{
+                id:user.id
+            }
+        });
+        res.status(201).json({msg: "User Update"})
+    } catch (error) {
+        res.status(500).json({msg: error.message + "none"});
+    }
 };
-export const deleteUser =(req,res)=>{
-
+export const deleteUser = async (req, res) => {
+const user = await User.findOne({
+    where: {
+        id: req.params.id
+    }
+});
+if (!user) return res.status(404).json({msg: "User tidak ditemukan"});
+try {
+    await User.destroy({
+        where:{
+            id: user.id
+        }
+    });
+    res.status(200).json({msg:"user Destroy"});
+} catch (error){
+    res.status(400).json({msg: error.message});
+}
 };
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 //import db from "./config/Datbase.js";
 import  UserRoute from "./routes/UserRoute.js";
 import  ProductRoute from "./routes/ProductRoute.js";
+import AuthRoute from "./routes/AuthRoute.js";
 
 dotenv.config();
 
@@ -23,11 +24,12 @@ app.use(session({
 }))
 app.use(cors({
     credentials:true,
-    origins: 'http:localhost:3000'
+    origins: 'http://localhost:5000'
 }));
 app.use(express.json());
 app.use(UserRoute);
 app.use(ProductRoute);
+app.use(AuthRoute);
 app.listen(process.env.APP_PORT,()=>{
     console.log('prendio esta monda')
 });
